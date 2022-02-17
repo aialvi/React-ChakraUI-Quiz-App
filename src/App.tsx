1f3314eae9992d4cf7bca48e96aea2d0607e781e
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-02-17 11:26:30 +0600

    add: login module

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,26 @@ import { NotFound } from "./components/misc/404";
 import { Home } from "./components/home/Home";
 import { Login } from "./components/auth/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/user" element={<User />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
